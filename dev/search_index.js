02bbef9611ae25033970d268a4e35511c35699fc
Documenter.jl <documenter@juliadocs.github.io>
2024-10-10 17:14:21 +0000

    build based on 5bbea70

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ElectricityMarketData","category":"page"},{"location":"#ElectricityMarketData","page":"Home","title":"ElectricityMarketData","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ElectricityMarketData.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ElectricityMarketData]","category":"page"}]
+}
