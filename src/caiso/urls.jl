1b9ffe055150349c359573ecdc428d14e6337838
Thiago Novaes <thiagonovaesb@poli.ufrj.br>
2024-10-30 11:36:24 +0100

    delete

--- src/caiso/urls.jl
+++ src/caiso/urls.jl
@@ -26,28 +26,3 @@ function _get_caiso_price_url(
 )::AbstractString
     return _get_caiso_url("PRC_INTVL_LMP", startdate, stopdate, market_run_id)
 end
-
-"""
-    _get_caiso_real_time_url(startdate::AbstractString, stopdate::AbstractString)::AbstractString
-
-Returns the real time url for the Caiso API.
-"""
-function _get_caiso_real_time_url(
-    startdate::AbstractString,
-    stopdate::AbstractString,
-)::AbstractString
-    return _get_caiso_price_url(startdate, stopdate, "RTM")
-end
-
-"""
-    _get_caiso_day_ahead_url(startdate::AbstractString, stopdate::AbstractString)::AbstractString
-
-Returns the day ahead url for the Caiso API.
-"""
-function _get_caiso_day_ahead_url(
-    startdate::AbstractString,
-    stopdate::AbstractString,
-)::AbstractString
-    return _get_caiso_price_url(startdate, stopdate, "DAM")
-end
-
--- test/caiso/urls.jl
+++ test/caiso/urls.jl
@@ -21,18 +21,4 @@
         @test url ==
               "http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=PRC_INTVL_LMP&version=1&startdatetime=20230104T07:00-0000&enddatetime=20230105T07:00-0000&market_run_id=RTM"
     end
-    @testset "_get_caiso_real_time_url" begin
-        startdate = "20230104T07:00-0300"
-        stopdate = "20230105T07:00-0300"
-        url = ElectricityMarketData._get_caiso_real_time_url(startdate, stopdate)
-        @test url ==
-              "http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=PRC_INTVL_LMP&version=1&startdatetime=20230104T07:00-0300&enddatetime=20230105T07:00-0300&market_run_id=RTM"
-    end
-    @testset "_get_caiso_day_ahead_url" begin
-        startdate = "20230104T07:00-0300"
-        stopdate = "20230105T07:00-0000"
-        url = ElectricityMarketData._get_caiso_day_ahead_url(startdate, stopdate)
-        @test url ==
-              "http://oasis.caiso.com/oasisapi/SingleZip?resultformat=6&queryname=PRC_INTVL_LMP&version=1&startdatetime=20230104T07:00-0300&enddatetime=20230105T07:00-0000&market_run_id=DAM"
-    end
 end
